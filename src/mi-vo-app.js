05991ee924d6646347defb38a557bfda9a4b167b
Ovi <ovidiu.mihota@yahoo.com>
2016-07-21 18:19:52 +0300

    first commit

--- src/mi-vo-app.js
+++ src/mi-vo-app.js
@@ -1,17 +1,27 @@
 import {Component, View} from 'angular2/core';
 
 @Component({
-  selector: 'mi-vo-app'
+    selector: 'mivo',
+    template: `
+  <div className="mivo-main">
+   <div class="header">
+     <h1>Header</h1>
+   </div>
+   <div class="mivo-content">
+    <h2>Content</h2>
+   </div>
+  </div>
+  `
 })
 
 @View({
-  templateUrl: 'mi-vo-app.html'
+    templateUrl: 'mi-vo-app.html'
 })
 
 export class MiVoApp {
 
-  constructor() {
-    console.info('MiVoApp Component Mounted Successfully');
-  }
+    constructor() {
+        console.info('MiVoApp Component Mounted Successfully');
+    }
 
 }
